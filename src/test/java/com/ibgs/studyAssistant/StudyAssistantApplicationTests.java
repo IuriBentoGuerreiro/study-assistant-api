be2a3f68ac1ea749485eb8d2fe1b3273eb62089f
Iuri Guerreiro <bentoiuri@gmail.com>
2025-12-12 09:06:46 -0300

    primeiro commit

---
+++ src/test/java/com/ibgs/studyAssistant/StudyAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibgs.studyAssistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
