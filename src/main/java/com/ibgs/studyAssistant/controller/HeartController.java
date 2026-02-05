b62e1ba14640d8fb6cabe376afdbabc32d527540
Iuri Guerreiro <bentoiuri@gmail.com>
2026-02-05 08:35:45 -0300

    fix: correção na permissão do endpoint /heart, pois estava como POST, mas ele é um GET

--- src/main/java/com/ibgs/studyAssistant/controller/HeartController.java
+++ src/main/java/com/ibgs/studyAssistant/controller/HeartController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HealthController {
+public class HeartController {
 
-    @GetMapping("/health")
+    @GetMapping("/heart")
     public String health() {
         return "OK";
     }
